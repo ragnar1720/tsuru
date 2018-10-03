5370decd7dd638fe10d7dd262c16802ac4eb13d8
Fabio <fabio@localhost.localdomain>
2018-10-03 18:12:03 -0300

    Fire in the hole

--- src/main/java/com/example/tsuru/tsuru.java
+++ src/main/java/com/example/tsuru/tsuru.java
@@ -25,7 +25,7 @@ public class tsuru extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Tsuru is coming");
+		response.getWriter().append("Tsuru is COMINGO");
 	}
 
 	/**
